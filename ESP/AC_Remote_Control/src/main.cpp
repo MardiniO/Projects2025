6a01b91d6ba133edcb01d70481c7ff9ee0debe8c
MardiniO <mardiniomar3@gmail.com>
2025-01-25 14:52:36 +0200

    Changed to Arduino framework

---
+++ ESP/AC_Remote_Control/src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+
+// Pin Definitions
+int ledPin = 22;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(1000);
+  Serial.print("Loop completed.");
+}
+
